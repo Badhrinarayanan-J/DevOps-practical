74af9fd42087ddd605baa4b6ff756b6154336e45
Badhrinarayanan-J <badhrinarayanan57@gmail.com>
2026-06-22 09:45:43 +0000

    Added Prometheus monitoring

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from datetime import datetime
+from prometheus_client import Counter, generate_latest
 
 app = Flask(__name__)
 
@@ -16,6 +17,10 @@ def health():
     return {
         "status": "UP"
     }
+
+@app.route("/metrics")
+def metrics():
+    return generate_latest()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
